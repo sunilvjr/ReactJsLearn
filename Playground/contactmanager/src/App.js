f6b485fdada0021bda8ad96fa46c1a4ab7a547e5
neilbakirtzis <127197683+neilbakirtzis@users.noreply.github.com>
2023-07-31 18:08:59 +0545

    End of Contact Manager

--- Playground/contactmanager/src/App.js
+++ Playground/contactmanager/src/App.js
@@ -4,30 +4,42 @@ import Wrapper from "./Wrapper";
 import "./styles/app.css";
 import ContactAdder from "./components/ContactAdder";
 import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   const initialContacts = [
-    { name: "John", telephone: "9841000000", country: "Nepal" },
-    { name: "Dave", telephone: "9841000001", country: "China" },
-    { name: "Smith", telephone: "9841000002", country: "India" },
+    // { name: "John", telephone: "9841000000", country: "Nepal" },
+    // { name: "Dave", telephone: "9841000001", country: "China" },
+    // { name: "Smith", telephone: "9841000002", country: "India" },
   ];
 
-  const [contacts, setContacts] = useState(initialContacts);
+  const getAllContacts = JSON.parse(localStorage.getItem("contacts"));
+  const [contacts, setContacts] = useState(
+    getAllContacts ? getAllContacts : initialContacts
+  );
 
   const addContactData = (contactData) => {
     console.log(contactData);
     //contacts.push(contactData);
-    setContacts([...contacts, contactData]);
+    const allContacts = [...contacts, contactData];
+    setContacts(allContacts);
     //setContacts([contactData, ...contacts]);
+    localStorage.setItem("contacts", JSON.stringify(allContacts));
     console.log(contacts);
+  };
+
+  const ClearAllContacts = () => {
+    localStorage.clear();
   };
 
   const childFunctionHandler = () => {
     alert("Garent Parent was called");
+    setContacts([]);
   };
 
   return (
     <>
+      <NavBar></NavBar>
       <div className="contact_adder">
         {/* Child to Parent Communication */}
         <ContactAdder
@@ -37,8 +49,8 @@ const App = () => {
       </div>
       <Wrapper>
         {/* Parent to Child Communication */}
-        {contacts.map((data) => (
-          <Contact data={data}></Contact>
+        {contacts.map((data, index) => (
+          <Contact key={index} data={data}></Contact>
         ))}
         {/* <Contact data={contacts[0]} />
         <Contact data={contacts[1]} />
@@ -46,6 +58,13 @@ const App = () => {
         {/* <Contact name="John" telephone="9841000000" country="Nepal" />
         <Contact name="Dave" telephone="9841000001" country="Nepal" />
         <Contact name="Smith" telephone="9841000002" country="Nepal" /> */}
+
+        <button
+          onClick={ClearAllContacts}
+          style={{ background: "#f00", color: "#fff" }}
+        >
+          Clear All Contacts
+        </button>
       </Wrapper>
     </>
   );
--- Playground/contactmanager/src/components/ContactAdder.js
+++ Playground/contactmanager/src/components/ContactAdder.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Dummy from "./Dummy";
+//import Dummy from "./Dummy";
 // import { Chris, Dave, John } from "./Person";
 
 const ContactAdder = (props) => {
@@ -7,20 +7,29 @@ const ContactAdder = (props) => {
   const [telephone, setTelephone] = useState("");
   const [country, setCountry] = useState("");
 
-  const onClickHandler = () => {
+  const onFormSubmitHandler = (e) => {
+    e.preventDefault();
     //console.log("clicked");
 
     //const contactData = { name: name, telephone: telephone, country: country };
-    const contactData = { name, telephone, country };
+    const contactData = { id: Math.random(), name, telephone, country };
     //console.log(contactData);
     //setName("Chris");
-    props.onContactAdded(contactData);
+    if (name.length < 1 || telephone.length < 1 || country < 1) {
+      alert("Please fill all the fields");
+    } else {
+      props.onContactAdded(contactData);
+      //Clearing the input fields
+      setName("");
+      setTelephone("");
+      setCountry("");
+    }
   };
 
-  const dummyClickHandler = () => {
-    //alert("Parent was called");
-    props.ChildFunction();
-  };
+  // const dummyClickHandler = () => {
+  //   //alert("Parent was called");
+  //   props.ChildFunction();
+  // };
 
   // let resultCard;
   // if (name === "John") {
@@ -33,28 +42,33 @@ const ContactAdder = (props) => {
   return (
     <>
       <div className="simpleAdder">
-        <input
-          type="text"
-          value={name}
-          placeholder="Contact Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={telephone}
-          placeholder="Telephone"
-          onChange={(e) => setTelephone(e.target.value)}
-        />
-        <input
-          type="text"
-          value={country}
-          placeholder="Country"
-          onChange={(e) => setCountry(e.target.value)}
-        />
-        <br />
-        <br />
-        <button onClick={onClickHandler}>Add Contact</button>
-        <div>{/* My name is {name}.{resultCard} */}</div>
+        <div>
+          <h3>Add Contact</h3>
+        </div>
+        <form onSubmit={onFormSubmitHandler}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Contact Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            value={telephone}
+            placeholder="Telephone"
+            onChange={(e) => setTelephone(e.target.value)}
+          />
+          <input
+            type="text"
+            value={country}
+            placeholder="Country"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <br />
+          <br />
+          <button>Add Contact</button>
+          <div>{/* My name is {name}.{resultCard} */}</div>
+        </form>
       </div>
       {/* <Dummy onDumyClicked={dummyClickHandler} /> */}
     </>
