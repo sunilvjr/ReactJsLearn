11902e7c5adfbd8398b9711f450c05cf1398682b
neilbakirtzis <127197683+neilbakirtzis@users.noreply.github.com>
2023-08-09 14:54:59 +0545

    Update App.js

--- Playground/bootstrap_test/src/App.js
+++ Playground/bootstrap_test/src/App.js
@@ -1,28 +1,7 @@
 import { Badge, Container, Navbar } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src="/assets/logo192.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
-            Bootstrap Testing
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-      Bootstrap testing...{" "}
-      <h1>
-        Example heading <Badge bg="secondary">New</Badge>
-      </h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
